b0b7cadf3b3f4b5a87832fbf37cb7233d56d339e
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2022-07-20 20:27:26 +0100

    fixed db connection

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -8,6 +8,7 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.30.0/mod.ts";
   await client.connect(dbString)
   
  console.log("Database connected!");
+ 
  const db = client.database("deno_auth");
 
  export default db;
